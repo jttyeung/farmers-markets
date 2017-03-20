cbdef839cfba40bacde9abc294b195f5ff208760
Joanne Yeung <jttyeung@gmail.com>
2017-03-19 22:02:47 -0700

    added static data folder and data

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect
-from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
+from flask_restful import reqparse, abort, Resource, Api
 from flask_debugtoolbar import DebugToolbarExtension
 import jinja2
 
@@ -12,27 +11,42 @@ api = Api(app)
 app.jinja_env.undefined = jinja2.StrictUndefined
 app.jinja_env.auto_reload = True
 
+
 @app.route('/')
 def homepage():
     """ Brings user to the homepage. """
     return render_template('index.html')
 
 
-@app.route('/api')
-def api_details():
-    """ Brings user to the API. """
-    return render_template('api.html')
+def error_if_nonexistent(fm_id):
+    # markets =
+    if fm_id not in markets:
+        abort(404, message='Farmer\'s Market {} not found'.format(fm_id))
 
-class Markets(Resource):
+
+class MarketList(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(Markets, '/markets')
 
-# @app.route('/api/markets')
+class Market(Resource):
+    def get(self, fm_id):
+        error_if_nonexistent(fm_id)
+        # return markets[fm_id]
+
+    def delete(self, fm_id):
+        error_if_nonexistent(fm_id)
+        return '', 204
+
+    def put(self, fm_id):
+        error_if_nonexistent(fm_id)
+        # return
 
 
-# @app.route('/api/markets/<fmid>')
+
+# Set up the API resource routing
+api.add_resource(MarketList, '/markets')
+api.add_resource(Markets, '/markets/<fm_id>')
 
 
 # @app.route('/api/markets/<state>')
