02f860aa690ce8b7da98f405317b58725b7bde4e
Joanne Yeung <jttyeung@gmail.com>
2017-03-21 23:08:26 -0700

    added exception for non-int edge case

--- server.py
+++ server.py
@@ -26,10 +26,13 @@ def error_if_nonexistent_fm_id(fm_id):
 
     fm_ids = get_market_ids()
 
-    # Convert URL query string to INT to compare to fm_ids
-    if int(fm_id) not in fm_ids:
-        abort(404, message='Farmer\'s market {} not found'.format(fm_id))
-
+    try:
+        # Convert URL query string to INT to compare to fm_ids
+        if int(fm_id) not in fm_ids:
+            abort(404, message='Farmer\'s market {} not found'.format(fm_id))
+    except:
+        # Catches non-integer fm_id requests
+        abort(404, message='Farmer\'s market {} is not a valid format. Use digits only.'.format(fm_id))
 
 def error_if_nonexistent_state_id(state_id):
     """ Returns an error message if state id is not found. """
